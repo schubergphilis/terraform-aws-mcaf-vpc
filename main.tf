77cf6269185093eda45c3f846f03c4e5e1782e4e
Sander van Harmelen <sander@vanharmelen.nl>
2019-07-25 16:22:26 +0200

    Remove unneeded quotes

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
   zones  = length(var.availability_zones)
 }
 
-resource "aws_vpc" "default" {
+resource aws_vpc default {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -11,12 +11,12 @@ resource "aws_vpc" "default" {
   tags                 = merge(var.tags, { "Name" = "${var.stack}-vpc" })
 }
 
-resource "aws_internet_gateway" "default" {
+resource aws_internet_gateway default {
   vpc_id = aws_vpc.default.id
   tags   = merge(var.tags, { "Name" = "${var.stack}-igw" })
 }
 
-resource "aws_eip" "nat" {
+resource aws_eip nat {
   count = local.zones
   vpc   = true
 
@@ -24,10 +24,10 @@ resource "aws_eip" "nat" {
     var.tags, { "Name" = "${var.stack}-nat-${local.az_ids[count.index]}" }
   )
 
-  depends_on = ["aws_internet_gateway.default"]
+  depends_on = [aws_internet_gateway.default]
 }
 
-resource "aws_nat_gateway" "default" {
+resource aws_nat_gateway default {
   count         = local.zones
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
@@ -37,7 +37,7 @@ resource "aws_nat_gateway" "default" {
   )
 }
 
-resource "aws_subnet" "public" {
+resource aws_subnet public {
   count                   = local.zones
   cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
   availability_zone       = var.availability_zones[count.index]
@@ -49,7 +49,7 @@ resource "aws_subnet" "public" {
   )
 }
 
-resource "aws_subnet" "private" {
+resource aws_subnet private {
   count                   = local.zones
   cidr_block              = cidrsubnet(var.cidr_block, 8, count.index + local.zones)
   availability_zone       = var.availability_zones[count.index]
@@ -61,24 +61,24 @@ resource "aws_subnet" "private" {
   )
 }
 
-resource "aws_route_table" "public" {
+resource aws_route_table public {
   vpc_id = aws_vpc.default.id
   tags   = merge(var.tags, { "Name" = "${var.stack}-public" })
 }
 
-resource "aws_route" "public" {
+resource aws_route public {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.default.id
 }
 
-resource "aws_route_table_association" "public" {
+resource aws_route_table_association public {
   count          = local.zones
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table" "private" {
+resource aws_route_table private {
   count  = local.zones
   vpc_id = aws_vpc.default.id
 
@@ -87,14 +87,14 @@ resource "aws_route_table" "private" {
   )
 }
 
-resource "aws_route" "private" {
+resource aws_route private {
   count                  = local.zones
   route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.default[count.index].id
 }
 
-resource "aws_route_table_association" "private" {
+resource aws_route_table_association private {
   count          = local.zones
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
