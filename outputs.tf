8e887ed6c97619a7a9c997e90499e504f2624102
Sander van Harmelen <sander@vanharmelen.nl>
2019-08-02 21:48:10 +0200

    The quotes aren't needed, but lots of tools expect them

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,39 @@
-output id {
+output "id" {
   value       = aws_vpc.default.id
   description = "ID of the VPC"
 }
 
-output cidr_block {
+output "cidr_block" {
   value       = aws_vpc.default.cidr_block
   description = "CIDR block of the VPC"
 }
 
-output igw_id {
+output "igw_id" {
   value       = aws_internet_gateway.default.id
   description = "ID of the Internet Gateway"
 }
 
-output nat_gateway_ids {
+output "nat_gateway_ids" {
   value       = aws_nat_gateway.default[*].id
   description = "IDs of the NAT gateways"
 }
 
-output public_subnet_ids {
+output "public_subnet_ids" {
   value       = aws_subnet.public[*].id
   description = "IDs of the public subnets"
 }
 
-output public_route_table_id {
+output "public_route_table_id" {
   value       = aws_route_table.public.id
   description = "ID of the public route table"
 }
 
-output private_subnet_ids {
+output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
   description = "IDs of the private subnets"
 }
 
-output private_route_table_ids {
+output "private_route_table_ids" {
   value       = aws_route_table.private[*].id
   description = "IDs of the private route tables"
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
-variable stack {
+variable "stack" {
   type        = string
   description = "The stack name for the VPC"
 }
 
-variable cidr_block {
+variable "cidr_block" {
   type        = string
   description = "The CIDR block for the VPC"
 }
 
-variable availability_zones {
+variable "availability_zones" {
   type        = list(string)
   description = "A list of availability zones for the subnets"
 }
 
-variable tags {
+variable "tags" {
   type        = map(string)
   description = "A mapping of tags to assign to the resources"
 }
