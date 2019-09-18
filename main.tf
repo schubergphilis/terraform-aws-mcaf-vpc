234e3f7d00005d5ee2bdfbf7162e34bb763faf32
Sander van Harmelen <sander@vanharmelen.nl>
2019-09-18 18:58:14 +0200

    This commit makes sure the correct index is used

--- main.tf
+++ main.tf
@@ -70,8 +70,8 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_subnet" "lambda" {
-  count                   = local.lambda_subnet_count
-  cidr_block              = cidrsubnet(var.cidr_block, local.lambda_subnet_newbits, count.index + local.zones)
+  count                   = local.lambda_subnets
+  cidr_block              = cidrsubnet(var.cidr_block, local.lambda_subnet_newbits, count.index + (local.zones * 2))
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = false
   vpc_id                  = aws_vpc.default.id
