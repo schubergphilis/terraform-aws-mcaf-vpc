1588dc321d5380becc3475ffa544bd80ea183100
Egon Braun <ebraun@schubergphilis.com>
2020-02-27 14:40:29 +0100

    Adds examples

---
+++ examples/public/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  version = "~> 2.50"
+  region  = "eu-west-1"
+}
+
+module "public_vpc" {
+  source              = "../../"
+  stack               = "test"
+  cidr_block          = "10.10.0.0/19"
+  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_only        = false
+  lambda_subnets      = false
+  private_subnet_bits = 22
+
+  tags = {
+    environment = "test"
+  }
+}
+
+module "public_vpc_with_lambdas" {
+  source              = "../../"
+  stack               = "test"
+  cidr_block          = "10.11.0.0/19"
+  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_only        = false
+  lambda_subnets      = true
+  private_subnet_bits = 22
+  lambda_subnet_bits  = 22
+
+  tags = {
+    environment = "test"
+  }
+}
