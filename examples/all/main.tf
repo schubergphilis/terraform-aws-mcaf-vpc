030a77bb6a45e99cf12d8bdaed009f4c41dd76f0
ghawker <ghawker@schubergphilis.com>
2020-06-30 14:01:07 +0200

    fixed examples

--- examples/all/main.tf
+++ examples/all/main.tf
@@ -30,38 +30,6 @@ module "full_vpc_with_lambda" {
   }
 }
 
-provider "aws" {
-  version = "~> 2.50"
-  region  = "eu-west-1"
-}
-
-module "full_vpc" {
-  source              = "../../"
-  name                = "test"
-  cidr_block          = "192.168.0.0/24"
-  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  public_subnet_bits  = 28
-  private_subnet_bits = 28
-
-  tags = {
-    environment = "test"
-  }
-}
-
-module "full_vpc_with_lambda" {
-  source              = "../../"
-  name                = "test"
-  cidr_block          = "192.168.1.0/24"
-  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  public_subnet_bits  = 28
-  private_subnet_bits = 28
-  lambda_subnet_bits  = 28
-
-  tags = {
-    environment = "test"
-  }
-}
-
 module "full_vpc_with_s3_endpoint" {
   source              = "../../"
   name                = "test"
@@ -74,21 +42,7 @@ module "full_vpc_with_s3_endpoint" {
     environment = "test"
   }
 }
-
-module "full_vpc_with_lambda" {
-  source              = "../../"
-  name                = "test"
-  cidr_block          = "192.168.1.0/24"
-  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  public_subnet_bits  = 28
-  private_subnet_bits = 28
-  lambda_subnet_bits  = 28
-
-  tags = {
-    environment = "test"
-  }
-}
-
+ 
 module "full_vpc_with_ssm_endpoints" {
   source              = "../../"
   name                = "test"
