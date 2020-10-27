f35a9cbf4b74ad1c071c86220b21c0df7e09a911
ghawker <ghawker@schubergphilis.com>
2020-10-27 09:25:33 +0100

    remove sec group outputs

--- outputs.tf
+++ outputs.tf
@@ -38,22 +38,6 @@ output "lambda_route_table_ids" {
   description = "IDs of the Lambda route tables"
 }
 
-output "ec2_endpoint_security_group_ids" {
-  value = length(var.ec2_endpoint.security_group_ids) == 0 ? aws_security_group.ec2_endpoint.*.id : var.ec2_endpoint.security_group_ids
-}
-
-output "ec2messages_endpoint_security_group_ids" {
-  value = length(var.ec2messages_endpoint.security_group_ids) == 0 ? aws_security_group.ec2messages_endpoint.*.id : var.ec2messages_endpoint.security_group_ids
-}
-
-output "ssm_endpoint_security_group_ids" {
-  value = length(var.ssm_endpoint.security_group_ids) == 0 ? aws_security_group.ssm_endpoint.*.id : var.ssm_endpoint.security_group_ids
-}
-
-output "ssmmessages_endpoint_security_group_ids" {
-  value = length(var.ssmmessages_endpoint.security_group_ids) == 0 ? aws_security_group.ssmmessages_endpoint.*.id : var.ssmmessages_endpoint.security_group_ids
-}
-
 output "public_subnet_ids" {
   value       = aws_subnet.public[*].id
   description = "IDs of the public subnets"
