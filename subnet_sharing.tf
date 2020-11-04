e796963dbec96e9b55ef453ce11b9fbddc89d958
Costas Tyfoxylos <costas.tyf@gmail.com>
2020-11-04 11:20:01 +0100

    Moved subnet sharing to module for reusability

--- subnet_sharing.tf
+++ subnet_sharing.tf
@@ -1,21 +1,9 @@
-resource "aws_ram_resource_share" "subnet_sharing" {
-  count                     = var.share_public_subnets || var.share_private_subnets ? 1 : 0
-  name                      = "${var.prepend_resource_type ? "resource-share-" : ""}subnets-${var.name}"
-  allow_external_principals = true
-  tags = merge(
-    { "environment" = var.name, "resource-type" = "ec2:Subnet" },
-    var.subnet_sharing_custom_tags
-  )
-}
-
-resource "aws_ram_resource_association" "private_subnets" {
-  for_each           = var.share_private_subnets ? toset(aws_subnet.private[*].arn) : []
-  resource_arn       = each.value
-  resource_share_arn = aws_ram_resource_share.subnet_sharing[0].arn
-}
-
-resource "aws_ram_resource_association" "public_subnets" {
-  for_each           = var.share_public_subnets ? toset(aws_subnet.public[*].arn) : []
-  resource_arn       = each.value
-  resource_share_arn = aws_ram_resource_share.subnet_sharing[0].arn
+module "subnet_sharing" {
+  source                = "git@git@github.com:schubergphilis/terraform-aws-mcaf-subnet-sharing.git?ref=v0.1.0"
+  count                 = var.share_public_subnets || var.share_private_subnets ? 1 : 0
+  name                  = var.name
+  private_subnet_arns   = var.share_private_subnets ? toset(aws_subnet.private[*].arn) : []
+  public_subnet_arns    = var.share_public_subnets ? toset(aws_subnet.public[*].arn) : []
+  prepend_resource_type = var.prepend_resource_type
+  tags                  = var.subnet_sharing_custom_tags
 }
