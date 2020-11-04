471521e92ea586651737dd9e0b8e448b382bb4a7
Costas Tyfoxylos <costas.tyf@gmail.com>
2020-11-04 13:37:36 +0100

    ordering of variables

--- subnet_sharing.tf
+++ subnet_sharing.tf
@@ -2,8 +2,8 @@ module "subnet_sharing" {
   source                = "git@github.com:schubergphilis/terraform-aws-mcaf-subnet-sharing.git?ref=v0.2.0"
   count                 = var.share_public_subnets || var.share_private_subnets ? 1 : 0
   name                  = var.name
+  prepend_resource_type = var.prepend_resource_type
   private_subnet_arns   = var.share_private_subnets ? toset(aws_subnet.private[*].arn) : []
   public_subnet_arns    = var.share_public_subnets ? toset(aws_subnet.public[*].arn) : []
-  prepend_resource_type = var.prepend_resource_type
   tags                  = var.subnet_sharing_custom_tags
 }
