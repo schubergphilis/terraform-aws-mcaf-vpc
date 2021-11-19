27ad3feda0d6d931423fa68b05667b2243a68db8
ghawker <ghawker@schubergphilis.com>
2021-11-19 13:49:33 +0100

    update naming for multiple or single route table on public subnets

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.default.id
   tags = merge(
     var.tags,
-    { "Name" = "${var.prepend_resource_type ? "route-table-" : ""}${var.name}-public" }
+    { "Name" = var.shared_public_route_table ? "${var.prepend_resource_type ? "route-table-" : ""}${var.name}-public" : "${var.prepend_resource_type ? "route-table-" : ""}${var.name}-public-${local.az_ids[count.index]}"}
   )
 }
 
