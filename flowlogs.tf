0a8a3ba3869029ac7a2d45681fb6a8009a98a8b9
Marko Skender <marko.skender@leaseplan.com>
2021-11-30 08:56:55 +0100

    Add support for permission boundary policies to Flow Logs Role

--- flowlogs.tf
+++ flowlogs.tf
@@ -17,13 +17,15 @@ data "aws_iam_policy_document" "log_stream_action" {
 
 module "flow_logs_role" {
   count                 = var.flow_logs != null ? 1 : 0
-  source                = "github.com/schubergphilis/terraform-aws-mcaf-role?ref=v0.3.0"
+  source                = "github.com/schubergphilis/terraform-aws-mcaf-role?ref=v0.3.2"
   name                  = var.flow_logs.iam_role_name
   principal_type        = "Service"
   principal_identifiers = ["vpc-flow-logs.amazonaws.com"]
   role_policy           = data.aws_iam_policy_document.log_stream_action.json
+  permissions_boundary =  var.flow_logs.iam_role_boundary_policy
   postfix               = var.postfix
   tags                  = var.tags
+
 }
 
 resource "aws_cloudwatch_log_group" "flow_logs" {
